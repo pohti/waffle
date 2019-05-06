2efc4e3cb7e621d282003f95702ea6b2401eb941
Poh <minmarnoo@gmail.com>
2019-05-06 12:36:42 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,11 @@
 <?php
 ob_start();
 
+$localhost="localhost";
+$mmo_home="192.168.174.171";
+
 try {
-  $con = new PDO("mysql:dbname=waffle;host=localhost", "root", "");
+  $con = new PDO("mysql:dbname=waffle;host=$mmo_home", "root", "");
   $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 }
 catch (PDOException $e){
